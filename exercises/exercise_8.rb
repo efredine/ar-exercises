c06d985fa8ca98207f3cb1e7890b02a02cf914a6
Eric Fredine <eric.fredine@gmail.com>
2016-12-06 22:39:38 +0000

    Complete exercise 8.

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -3,12 +3,10 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
 
-puts "Exercise 7"
+
+puts "Exercise 8"
 puts "----------"
 
 # Your code goes here ...
 p Employee.create!(first_name: "Eric", last_name: "Fredine", hourly_rate: 200, store_id: 2)
-p Store.create!(name: "abc", annual_revenue: 10, mens_apparel: true, womens_apparel: false)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,18 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
   validates :first_name, :last_name, :store, presence: true
   validates :hourly_rate,
     presence: true,
     numericality: { only_integer: true },
     inclusion: { in: 40..200 }
+
+  before_create :ensure_password_present
+
+  private
+
+  def ensure_password_present
+    self.password = "password"
+  end
+
 end
